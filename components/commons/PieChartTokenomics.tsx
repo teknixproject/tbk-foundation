cc584f4c9803e1aa08f6e1fafe1ab193e2d61e1d
nguyenthaibao.haugiang@gmail.com <170920443+ryannguyen2000@users.noreply.github.com>
2025-04-03 09:33:32 +0700

    Update PieChartTokenomics.tsx

--- components/commons/PieChartTokenomics.tsx
+++ components/commons/PieChartTokenomics.tsx
@@ -2,12 +2,13 @@
 'use client';
 
 import Image from "next/image";
-import { useMemo } from "react";
+import { CSSProperties, useMemo } from "react";
 import clsx from "clsx";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import { getDeviceType } from "@/lib/utils";
-import styled, { CSSProperties } from "styled-components";
+import styled from "styled-components";
+import _ from "lodash";
 
 export interface TokenomicsChartType {
   series: any[];
